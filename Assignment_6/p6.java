d8fd06a95cc68409201591296dde268dcb8167fc
ParthManna <mannapartha2004@gmail.com>
2024-09-15 17:39:02 +0530

    add assi_6

--- Assignment_6/p6.java
+++ Assignment_6/p6.java
@@ -1,13 +1,63 @@
-interface class Student{
-	int roll;
-	void getRoll();
-	void display();
+// Student class
+class Student {
+    protected int roll;
+
+    public void getRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public void displayRoll() {
+        System.out.println("Roll number: " + roll);
+    }
 }
 
-class Test{
+// Test class (inherits from Student)
+class Test extends Student {
+    protected float m1, m2;
 
-	float m1,m2;
-	void getmarks();
+    public void getMarks(float m1, float m2) {
+        this.m1 = m1;
+        this.m2 = m2;
+    }
 
+    public void displayMarks() {
+        System.out.println("Marks 1: " + m1);
+        System.out.println("Marks 2: " + m2);
+    }
+}
 
-	
+// Games Interface
+interface Games {
+    float gamewt = 8.0f;
+
+    void displayWT();
+}
+
+// Result class (inherits from Test and implements Games)
+class Result extends Test implements Games {
+
+    @Override
+    public void displayWT() {
+        System.out.println("Games weight: " + gamewt);
+    }
+
+    public void displayResult() {
+        displayRoll();
+        displayMarks();
+        displayWT();
+        float total = m1 + m2 + gamewt;
+        System.out.println("Total score: " + total);
+    }
+}
+
+// Main class to execute the program
+public class p6 {
+    public static void main(String[] args) {
+        Result student = new Result();
+        
+        student.getRoll(101);  // Providing roll number
+        student.getMarks(85.5f, 90.0f);  // Providing marks for two subjects
+
+        student.displayResult();  // Displaying result with total score
+    }
+}
